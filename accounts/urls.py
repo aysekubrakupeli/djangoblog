b21941de5832e22d23c8a7e5ca52879b896a9847
Ayse Kubra <aysekubrakupeli@gmail.com>
2017-11-15 15:25:07 +0000

    Commiting changes of start blog

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
-from .views import logout, login
-
+from .views import logout, login, register, profile
 
 urlpatterns = [
-    url(r'^logout', logout, name='logout'),
-    url(r'^login', login),
+    url(r'^logout', logout, name="logout"),
+    url(r'^login', login, name="ishmael"),
+    url(r'^register', register, name="register"),
+    url(r'profile', profile, name="profile"),
     ]
+    
+    
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,20 @@
-from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from django.contrib import messages, auth
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegistrationForm
+
 
 # Create your views here.
 def get_index(request):
     return render(request, 'index.html')
     
+@login_required(login_url='/accounts/login')
+def profile(request):
+    return render(request, "profile.html")
+    
 def logout(request):
     auth.logout(request)
-    messages.success(request, 'You have successfully logged out')
-    return redirect(reverse('index'))
+    return redirect(get_index)
     
 def login(request):
     if request.method=="POST":
@@ -21,13 +26,32 @@ def login(request):
 
             if user is not None:
                 auth.login(request, user)
+                messages.success(request, "You have successfully logged in")
+                return redirect(profile)
             else:
                 form.add_error(None, "Your username or password was not recognised")
-                
-            return redirect(get_index)
+    
     else:
         form = UserLoginForm()
     
     return render(request, "login.html", {'form': form})
+
+
+def register(request):
+    if request.method=="POST":
+        form = UserRegistrationForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            
+            user = auth.authenticate(username=form.cleaned_data['username'],
+                                     password=form.cleaned_data['password1'])
+                                     
+            if user is not None:
+                auth.login(request, user)
+                messages.error(request, "You have successfully logged in")
+                return redirect(profile)
+    else:
+        form = form.add_error(None, "Your username or passwords was not recognised")
     
-    
+    return render(request, "register.html", {'form': form})
