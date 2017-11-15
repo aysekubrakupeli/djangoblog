86a821fec959fff80de727e7ebfbc75fca0cb6f5
Ayse Kubra <aysekubrakupeli@gmail.com>
2017-11-15 12:36:38 +0000

    Initial commit + Beginning of blog

---
+++ accounts/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages, auth
+from .forms import UserLoginForm
+
+# Create your views here.
+def get_index(request):
+    return render(request, 'index.html')
+    
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'You have successfully logged out')
+    return redirect(reverse('index'))
+    
+def login(request):
+    if request.method=="POST":
+        form=UserLoginForm(request.POST)
+        
+        if form.is_valid():
+            user = auth.authenticate(username=form.cleaned_data['username'],
+                                     password=form.cleaned_data['password'])
+
+            if user is not None:
+                auth.login(request, user)
+            else:
+                form.add_error(None, "Your username or password was not recognised")
+                
+            return redirect(get_index)
+    else:
+        form = UserLoginForm()
+    
+    return render(request, "login.html", {'form': form})
+    
+    
